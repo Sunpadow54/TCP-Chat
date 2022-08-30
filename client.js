2ab8561b4e71ec817f17419cf9bee8f60b32c68d
elsa <elsa.dessarps@gmail.com>
2022-08-30 15:28:30 +0200

    Feat: Add username and password control to access the chat.

--- client.js
+++ client.js
@@ -2,6 +2,13 @@ const readLine = require('readline')
 const net = require('net')
 
 const DEFAULT_PORT = 8080
+const color = {
+    red : '\x1b[31m%s\x1b[0m',
+    green: '\x1b[32m%s\x1b[0m',
+    cyan: '\x1b[36m%s\x1b[0m',
+    yellow: '\x1b[33m%s\x1b[0m',
+    bgCyan : '\x1b[46m'
+}
 
 const rl = readLine.createInterface({
     input: process.stdin,
@@ -9,55 +16,70 @@ const rl = readLine.createInterface({
 })
 
 // ------------- Functions
-const waitUsername = new Promise((resolve) => {
-        rl.question('Entrer a username: ', (answer) => {
+const rlQuestions = (question) => {
+    return new Promise((resolve) => {
+        const type = question.split(' ').pop()
+        rl.question(question + ': ', (answer) => {
             if (answer !== '') resolve(answer)
             else {
-                console.log('\x1b[31m%s\x1b[0m','Your must enter a Username')
+                console.log(color.red,'Your must enter a ' + type)
                 rl.close()
             }
         })
-})
+    })
+}
+
+const login = async () => {
+    let authData = []
+
+    const username = await rlQuestions('Enter a Username')
+    const password = await rlQuestions('Enter the Password')
+
+    authData.push(username, password)
+
+    return Promise.resolve(authData)
+}
 
 // ---------------
 
-waitUsername
-    .then(username => {
-        const socket = net.connect({
-            port: DEFAULT_PORT
-        })
-        
-        // ---- Connection
-        socket.on('connect', () => {
-            console.log('\x1b[36m%s\x1b[0m', `Welcome ${username} !`)
-            socket.write(`${username} joined the chat.`)
-        })
+login()
+    .then((authData) => {
+        const [username, password] = authData
 
-        // ---- Read line
-        rl.on('line', data => {
-            if (data === '/quit') {
-                socket.write(`${username} has left the chat.`);
-                socket.end()
-            }
-            else {
-                socket.write(username + ': ' + data)
-            }
-        })
-        
-        // ---- handle msg recieved from server
-        socket.on('data', data => {
-            console.log('\x1b[32m%s\x1b[0m', data);
-        })
-        
-        // ---- Disconnection
-        socket.on('end', () => {
-            console.log('\x1b[36m%s\x1b[0m', 'Disconnected');
-            rl.close()
-        })
+        if (username && password) {
+            const socket = net.connect({
+                port: DEFAULT_PORT
+            })
 
-        // ---- Errors
-        socket.on('error', (err) => {
-            console.log('error server: ', err);
-        })
-        
+            socket.on('connect', () => {
+                socket.write(`LOGIN/username=${username}&password=${password}`)
+            })
+
+            // ---- Read line
+            rl.on('line', data => {
+                if (data === '/quit') {
+                    socket.write(`${username} has left the chat.`);
+                    socket.end()
+                }
+                else {
+                    socket.write(username + ': ' + data)
+                }
+            })
+            
+            // ---- handle msg recieved from server
+            socket.on('data', data => {
+                console.log(color.cyan, data)
+            })
+
+            // ---- Disconnection
+            socket.on('end', () => {
+                console.log(color.cyan, 'Disconnected')
+                rl.close()
+            })
+
+            // ---- Errors
+            socket.on('error', (err) => {
+                console.log('error server: ', err)
+            })
+        }
     })
--- server.js
+++ server.js
@@ -4,13 +4,64 @@ const server = net.createServer()
 const DEFAULT_PORT = 8080
 let users = []
 
+const AUTH_USER_REGEX = /^LOGIN\/username=(?<name>.+)&password=(?<pwd>.+)$/
 
 // ------------- Functions
+const sendError = (socket, msg) => {
+    socket.write(msg)
+    socket.end()
+}
+
+const isUniqueUsername = (string) => {
+    const result = users.some(user => user.username === string)
+    return !result
+}
+
+const quitChat = (socket) => {
+    // Remove client from users array
+    const clientIndex = users.indexOf(client)
+    users.splice(clientIndex, 1)
+    // Print server msg
+    showMessage('left')
+    // End client
+    socket.end()
+}
+
 const broadcast = (msg, clientSender) => {
     users.forEach(client => {
         if (client !== clientSender) {
-            client.write(msg);
+            client.write(msg)
         }
+    })
+}
+
+const authUser = (client) => {
+    client.once('data', (authData) => {
+        if (!AUTH_USER_REGEX.test(authData)) {
+            return sendError(client, 'Unauthorized')
+        }
+
+        const [wholeMatch, username, password] = AUTH_USER_REGEX.exec(authData)
+
+        if (!isUniqueUsername(username)) {
+            return sendError(client, 'Unauthorized')
+        }
+
+        if (password !== 'pass') {
+            return sendError(client, 'Unauthorized')
+        }
+
+        // Success ! 
+        // - Add User to Chat.
+        client.username = username
+        users.push(client)
+        showMessage('join')
+
+        // - Send Msg to the Chat
+        broadcast(`${username} join the chat !`, client)
+        client.write(`Welcome ${username}!`)
+
+        return
     })
 }
 
@@ -26,33 +77,25 @@ server.on('error', err => console.error(err.stack))
 
 // ------------- Server Connection
 server.on('connection', (client) => {
-    // Check username and password ?
-    users.push(client)
-
-    showMessage('join')
+    // ---- Check username and password
+    authUser(client)
 
     // ---- Data recieve
     client.on('data', data => {
-        // data sent
-        broadcast(data, client)
+        // If request is to quit the chat
+        if (users.indexOf(client) !== -1) {
+            if (data === '/quit') {
+                quitChat(client)
+            }
+            // If the data recieve is not a Login request
+            if (!AUTH_USER_REGEX.test(data)) {
+                broadcast(data, client)
+            }
+        }
     })
-
-    // ---- Close client
-    client.on('end', () => {
-        // Remove client from users array
-        const clientIndex = users.indexOf(client)
-        users.splice(clientIndex, 1)
-
-        // Close socket
-        client.end()
-
-        showMessage('left')
-    })
-    
 })
 
 // ------------- Listen to port
 server.listen(DEFAULT_PORT, () => {
 	console.log('Server listening on port %s', DEFAULT_PORT)
 })
-
