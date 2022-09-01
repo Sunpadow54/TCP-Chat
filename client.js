313003fca8cd50a4da6f48ef676c2fed39d39d49
elsa <elsa.dessarps@gmail.com>
2022-09-01 19:47:19 +0200

    Fix: Correct readline input mix when typing while recieving msg from the server.

--- client.js
+++ client.js
@@ -7,30 +7,34 @@ const DEFAULT_PORT = 8080
 const rl = readLine.createInterface({
     input: process.stdin,
     output: process.stdout,
+    prompt: '',
+    terminal: true,
 })
 
 const colors = {
     warning : '\x1b[1;31m%s\x1b[0m',
     auth: '\x1b[1;37;44m%s\x1b[0m\xa0',
-    userMsg: '\x1b[2;30m%s\xa0\x1b[36m%s\xa0\x1b[0m%s',
-    myMsg: '\x1b[2;32m%s\xa0\xa0\x1b[32m',
+    userMsg: '\x1b[%s\x1b[36m%s\xa0\x1b[0m%s',
+    myMsg: '\x1b[%s\x1b[32m%s\xa0\x1b[0m',
     info: '\x1b[36m%s\x1b[0m',
-    highlight: '\x1b[1;33m%s\x1b[36m'
+    highlight: '\x1b[1;33m%s\x1b[36m',
+    date: '\x1b[90m%s\xa0\x1b[0m',
 }
 
 
 // ------------------- Functions -------------------
 
-const getDate = () => {
-    const now = new Date()
-    return `[${now.toLocaleTimeString('fr-FR')}]`
-}
 
 const colorMsg = (data, color) => {
     if (Array.isArray(data)) {
         return util.format(colors[color], ...data)
     }
     return util.format(colors[color], data)
+}
+
+const getDate = () => {
+    const now = new Date()
+    return colorMsg(`[${now.toLocaleTimeString('fr-FR')}]`, 'date')
 }
 
 const rlQuestions = (question) => {
@@ -72,7 +76,7 @@ const login = async () => {
             }
             if (data.toString() === 'Unauthorized') {
                 console.log(colorMsg(data.toString(), 'warning'))
-                socket.end()
+                socket.destroy()
                 rl.close()
             }
         })
@@ -80,33 +84,54 @@ const login = async () => {
     })
 }
 
-// ------------------- Connect -------------------
+// ------------------- Chat -------------------
 
 login()
     .then(res => {
         const [socket, username] = res
 
-        
+        let isTyping = false
+
         // ---- Read line
         rl.on('line', data => {
+
             if (data === '/quit') {
                 socket.write(colorMsg(`>>>> ${username} has left the chat.`, 'info'))
                 socket.setTimeout(1000)
             }
             else {
+                // Send to server
                 const user = `<${username}>`
                 socket.write(colorMsg([getDate(), user, data], 'userMsg'))
+                // clear line and rewrite the formatted data
+                readLine.moveCursor(process.stdout, 0, -1)
+                console.log(getDate() + data)
             }
         })
-        
+
         // ---- Socket
         socket.on('data', data => {
+            if (isTyping) {
+                let keepText = rl.line
+                rl.write(null, {ctrl: true, name: 'u'})
                 console.log(data.toString())
+                rl.write(keepText)
+            } else {
+                console.log(data.toString())    
+            }
+                
         })
 
         socket.on('timeout', () => {
             socket.write('/QUIT')
             rl.close()
             socket.destroy()
-        });
+        })
+
+        process.stdin.on('keypress', (str, key) => {
+            isTyping = true
+            if (key.name === 'return') {
+                isTyping = false
+            }
+        })
     })
